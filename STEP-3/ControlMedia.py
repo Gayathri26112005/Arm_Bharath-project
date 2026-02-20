bca701c6f3d60151dcd6fe30a3f3036a9a986270
Gayathri26112005 <gayathrireddy2611@gmail.com>
2026-02-20 14:24:03 +0530

    Create ControlMedia.py

---
+++ STEP-3/ControlMedia.py
@@ -0,0 +1,19 @@
+pip3 install pyautogui
+
+Examples:
+pyautogui.press('space')   # Play / Pause
+pyautogui.press('volumeup')
+pyautogui.press('volumedown')
+pyautogui.press('forward')
+pyautogui.press('backward')
+
+Important Flow:
+Camera ON
+↓
+Detect Hand
+↓
+Count Fingers
+↓
+Match Gesture
+↓
+Control Media
